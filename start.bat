c704a08872a30ddb5a687249c922e2b8e25d047c
Alexander Troshin <alexander@ub3rfish.net>
2013-03-22 23:02:39 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,2 +1,8 @@
+@ECHO OFF
+echo ---------------------------------------------------
+echo Livebots NXT Tank by Nibbler (Zoidberrg @ GitHub)
+echo ---------------HAVE FUN----------------------------
+echo ~ PLEASE GIVE ATTRIBUTION (Required by the license ~
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 runner.py
 pause
