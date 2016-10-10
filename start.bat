36cca9f318d5704e2f50fe86e058e26839979db5
Alexander T <novium@users.noreply.github.com>
2016-10-10 13:50:00 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,8 +1,3 @@
 @ECHO OFF
-echo ---------------------------------------------------
-echo Livebots NXT Tank by Nibbler (Novium @ GitHub)
-echo ---------------HAVE FUN----------------------------
-echo ~ PLEASE GIVE ATTRIBUTION (Required by the license ~
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 runner.py
 pause
